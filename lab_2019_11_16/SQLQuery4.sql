7c23583218bd74eab09be0425fa7b09c3d63df97
pawe213 <pawe213@gmail.com>
2019-11-16 15:26:02 +0100

    lab2_2

--- lab_2019_11_16/SQLQuery4.sql
+++ lab_2019_11_16/SQLQuery4.sql
@@ -46,8 +46,84 @@
 --biblioteki (baza library). Interesuje nas imię, nazwisko i data
 --urodzenia dziecka.
 
-USE LIBRARY
-SELECT lastname, firstname, birth_date
-FROM juvenile
-Inner join member
-ON juvenile.member_no = member.member_no
+--USE LIBRARY
+--SELECT lastname, firstname, birth_date
+--FROM juvenile
+--Inner join member
+--ON juvenile.member_no = member.member_no
+
+--Napisz polecenie, które podaje tytuły aktualnie wypożyczonych
+--książek
+--SELECT title
+--FROM loan
+--Inner join title
+--ON loan.title_no = title.title_no
+
+--Podaj informacje o karach zapłaconych za przetrzymywanie książki
+--o tytule ‘Tao Teh King’. Interesuje nas data oddania książki, ile dni
+--była przetrzymywana i jaką zapłacono karę
+
+--USE Library
+--SELECT fine_paid,  in_date, due_date, datediff(day, in_date,due_date)
+--FROM loanhist
+--Inner join title
+--ON  title.title_no = loanhist.title_no
+--Where title = 'Tao Teh King' and due_date > in_date
+
+--Napisz polecenie które podaje listę książek (mumery ISBN)
+--zarezerwowanych przez osobę o nazwisku: Stephen A. Graff
+
+--SELECT r.isbn from reservation r
+--inner join member m on m.member_no = r.member_no
+--where m.lastname = 'Graff' and m.firstname = 'Stephen' and m.middleinitial = 'A'
+
+--USE joindb
+--SELECT buyer_name, qty
+--FROM buyers
+--CROSS JOIN sales
+
+--Napisz polecenie, które wyświetla listę dzieci będących członkami
+--biblioteki (baza library). Interesuje nas imię, nazwisko, data
+--urodzenia dziecka i adres zamieszkania dziecka.
+--USE library
+--Select lastname, firstname, birth_date, state, city, street
+--from juvenile j
+--inner join member m ON j.member_no = m.member_no
+--inner join adult a ON j.adult_member_no = a.member_no
+
+--Napisz polecenie, które wyświetla listę dzieci będących członkami
+--biblioteki (baza library). Interesuje nas imię, nazwisko, data
+--urodzenia dziecka, adres zamieszkania dziecka oraz imię i nazwisko
+--rodzica.
+
+--USE library
+--Select m.lastname, m.firstname, birth_date, state, city, street
+--from member m
+--inner join juvenile j ON j.member_no = m.member_no
+--inner join adult a ON j.adult_member_no = a.member_no
+--inner join member m2 ON m2.member_no = a.member_no
+
+--Łączenie tabeli samej ze sobą – self join
+--SELECT a.buyer_id AS buyer1, a.prod_id
+--,b.buyer_id AS buyer2
+--FROM sales AS a
+--JOIN sales AS b
+--ON a.prod_id = b.prod_id
+--WHERE a.buyer_id > b.buyer_id
+
+--Napisz polecenie, które wyświetla pracowników oraz ich
+--podwładnych (baza northwind)
+--USE northwind
+--SELECT Pracownik.LastName, Pracownik.FirstName, Pracownik.ReportsTo, Cheff.LastName 
+--from Employees as Cheff
+--JOIN Employees as Pracownik
+--on Cheff.EmployeeID = Pracownik.ReportsTo
+
+--Napisz polecenie, które wyświetla pracowników, którzy nie mają
+--podwładnych (baza northwind)--USE northwind
+--SELECT boss.LastName, boss.FirstName, boss.EmployeeID 
+--from Employees as empl
+--right outer join Employees as boss
+--on empl.ReportsTo = boss.EmployeeID--where empl.EmployeeID is Null--Podaj listę członków biblioteki mieszkających w Arizonie (AZ) mają
+--więcej niż dwoje dzieci zapisanych do bibliotekiUSE librarySELECT m.firstname, m.lastnameFROM member minner join adult aon m.member_no = a.member_nojoin juvenile as jon a.member_no = j.adult_member_nowhere a.state = 'AZ'group by m.firstname--having count(*) >2 
+
